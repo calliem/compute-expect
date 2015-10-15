6f92d4bf01f155f1a1bcb7cc49a7aa6c8c6b7b7b
Callie Mao <cym4@duke.edu>
2015-10-15 16:45:20 -0400

    Fixed FileReader with absolut path concatenation

--- src/main/java/Regression.java
+++ src/main/java/Regression.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,46 +13,34 @@ public class Regression {
     public static void main (String[] args) {
         int numTaxa = 659; // hardcoded temporarily
 
-        Map<String, Integer> profSizes = loadProfileSizes("/results/ProfileSizes.txt");
-        System.out.println(profSizes);
+        Map<String, Integer> profSizes = loadProfileSizes("/data/results/ProfileSizes.txt");
+        System.out.println("profsizes " + profSizes);
+        System.out.println("profsizes " + profSizes.get("http://www.informatics.jax.org/marker/MGI:95921"));
+
 
     }
 
-    /*
-     * public int[] getScores(String inputFile){
-     * Path resultsDir = inputFile;
-     * 
-     * 
-     * scores=[]
-     * geneprofilesizes=[]
-     * taxonprofilesizes=[]
-     * rawscores=[]
-     * 
-     * loadProfileSizes("../results/ProfileSizes.txt");
-     * return null;
-     * }
-     */
+  
 
     public static Map<String, Integer> loadProfileSizes (String inputSizesFile) {
         Map<String, Integer> profileSize = new HashMap<String, Integer>();
 
         BufferedReader input;
         try {
-            input = new BufferedReader(new FileReader(inputSizesFile));
+        	String absPath = new File("").getAbsolutePath();
+        	input = new BufferedReader(new FileReader(absPath + inputSizesFile));
+            
             String line = input.readLine();
             while (line != null)
             {
-                System.out.println(line);
-                line = input.readLine();
-
                 String[] splitString = line.trim().split("\t");
                 String entity = splitString[0];
                 entity = entity.replace("#profile", "");
                 entity = entity.replace("http://purl.obolibrary.org/obo/", "");
                 String size = splitString[1];
                 profileSize.put(entity, Integer.parseInt(size));
+                line = input.readLine();
             }
-            
             input.close();
         }
         catch (FileNotFoundException e) {
