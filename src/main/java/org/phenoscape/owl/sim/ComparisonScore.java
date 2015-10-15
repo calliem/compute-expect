706e009f30954842137d397fa37d0fe83878b7b0
Jim Balhoff <jim@balhoff.org>
2015-10-15 14:41:16 -0400

    Interfaces for Expect score computation.

---
+++ src/main/java/org/phenoscape/owl/sim/ComparisonScore.java
@@ -0,0 +1,13 @@
+package org.phenoscape.owl.sim;
+
+public interface ComparisonScore<ID> {
+
+	public ID id();
+
+	public double similarity();
+
+	public ID queryProfile();
+
+	public ID corpusProfile();
+
+}
