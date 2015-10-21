e763d01a57b3663d7507f9721d538d52952fae5e
Jim Balhoff <jim@balhoff.org>
2015-10-21 13:35:43 -0400

    Javadoc for Expect Score interfaces.

--- src/main/java/org/phenoscape/owl/sim/ComparisonScore.java
+++ src/main/java/org/phenoscape/owl/sim/ComparisonScore.java
@@ -1,13 +1,30 @@
 package org.phenoscape.owl.sim;
 
+/**
+ * @author jbalhoff
+ *
+ * @param <ID> The type used for an identifier (e.g. IRI, URI, String, etc.).
+ */
 public interface ComparisonScore<ID> {
 
+	/**
+	 * @return Unique identifier for this comparison
+	 */
 	public ID id();
 
+	/**
+	 * @return Similarity score for the query and corpus profiles being compared
+	 */
 	public double similarity();
 
+	/**
+	 * @return Identifier for the compared query profile
+	 */
 	public ID queryProfile();
 
+	/**
+	 * @return Identifier for the compared corpus profile
+	 */
 	public ID corpusProfile();
 
 }
--- src/main/java/org/phenoscape/owl/sim/ExpectScoreComputation.java
+++ src/main/java/org/phenoscape/owl/sim/ExpectScoreComputation.java
@@ -3,8 +3,19 @@ package org.phenoscape.owl.sim;
 import java.util.Collection;
 import java.util.Map;
 
+/**
+ * @author jbalhoff
+ *
+ * @param <ID> The type used for an identifier (e.g. IRI, URI, String, etc.).
+ */
 public interface ExpectScoreComputation<ID> {
 
+	/**
+	 * @param scores A collection of comparison score objects (e.g. all comparisons of gene profile by taxon profile).
+	 * @param corpusProfileSizes Map of profile identifier to profile size (count of phenotypes in profile), for all profiles in corpus. Size of this map is size of corpus.
+	 * @param queryProfileSizes Map of profile identifier to profile size (count of phenotypes in profile), for all profiles used as queries.
+	 * @return Map of ComparisonScore identifiers to computed Expect Score.
+	 */
 	public Map<ID, Double> computeExpectScores(Collection<ComparisonScore<ID>> scores, Map<ID, Integer> corpusProfileSizes, Map<ID, Integer> queryProfileSizes);
 
 }
